fbe3cb1db87d0e2396259d1c8a00b160b3eeb82b
boll84011336 <boll84011336@gmail.com>
2023-01-23 22:59:28 +0800

    將 package.json移至根目錄

--- Restaurant/public/scripts/faq.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.section__FAQ').addEventListener('click', (e) => {
-  e.target.closest('.section__Q-And-A').querySelector('.section__question').nextElementSibling.classList.toggle('fold')
-})
